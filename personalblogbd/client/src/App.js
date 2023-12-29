6ae7bc2250ab8576546e1b9de1f424f60ba89acd
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2023-12-29 10:23:50 +0600

    blog

---
+++ personalblogbd/client/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img src="image\Easy(6).png " alt="" />
+        <h2>Watermelon Blog</h2>
+        <p>
+          WOW so good and testy looks awesome having fun with this water melon
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;

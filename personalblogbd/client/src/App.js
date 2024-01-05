de08be9db7573c993698a95199e60190c418905d
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-01-05 19:20:29 +0600

    ps

--- personalblogbd/client/src/App.js
+++ personalblogbd/client/src/App.js
@@ -2,20 +2,18 @@ import "./App.css";
 import Post from "./Post";
 import Header from "./Header";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/> } />
+      </Route>
     </Routes>
   );
 }
